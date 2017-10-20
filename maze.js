968b1fa95d2723d4ada48dc857505f45a295e562
aquaricky3 <rickyndatsjusme@gmail.com>
2017-10-20 00:03:01 -0700

    Exercise 2 completed Signed-off-by: aquaricky3 <rickyndatsjusme@gmail.com>

--- maze.js
+++ maze.js
@@ -1,6 +1,13 @@
 window.onload = function () {
-  var border1 = document.getElementById("boundary1");
-  border1.addEventListener("mouseover", function turnRed() {
-     border1.setAttribute("class", "boundary youlose");
-    });
+  var border = document.querySelectorAll("div#maze div.boundary");
+  for (var i = 0; i < border.length; i++) {
+    border[i].addEventListener("mouseover", turnRed);
+  }
 };
+
+function turnRed() {
+  var border = document.querySelectorAll("div#maze div.boundary");
+  for (var i = 0; i < border.length; i++) {
+    border[i].setAttribute("class", "boundary youlose");
+  }
+}
